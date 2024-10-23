b0b820a15417492983bf1c064f3cf64028e34f67
TDVCool123 <luisandypp@gmail.com>
2024-10-23 18:12:34 -0400

    Commit nro 3

---
+++ src/test/java/api/testSuite/FourProjectCreatedTest.java
@@ -0,0 +1,44 @@
+package api.testSuite;
+
+import config.ApiProjectConfig;
+import factoryRequest.FactoryRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+
+public class FourProjectCreatedTest extends BasicAuthTestBase{
+    @Test
+    @DisplayName("Verify the create an user later a project - todo.ly")
+    public void create4Projects() {
+
+        //Create 4 Projects
+        for (int i = 1;i <=4;i++) {
+            request.setUrl(ApiProjectConfig.CREATE_PROJECT);
+            request.setHeaders(auth, valueAuth);
+
+            body.put("Content", "Project "+i);
+
+            request.setBody(body.toString());
+
+            response = FactoryRequest.make("post").send(request);
+            response.then().statusCode(200).body("Content", equalTo(body.get("Content")));
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("Delete all project - todo.ly")
+    public void deleteAllProject() {
+        //get all proyects
+        request.setUrl(ApiProjectConfig.READ_ALL_PROJECTS);
+
+
+
+    }
+}
